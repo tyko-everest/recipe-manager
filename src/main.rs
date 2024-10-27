57d6c7fa51a555d06e954a4a0b8053b8d5e52eef
Tyko van Vliet <tykovv@gmail.com>
2024-10-27 13:32:02 -0600

    recursive ingredient rendering

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use askama::Template;
 use fraction::Fraction;
-use std::fs;
+use std::{fmt::{self, write}, fs, path::Display};
 use serde::{Serialize, Deserialize};
 use serde_yaml;
 
@@ -23,6 +23,8 @@ struct Recipe {
 fn render_recipe() -> Result<(), Box<dyn std::error::Error>>  {
     let recipe = fs::read_to_string("recipes/simple.yaml")?;
     let recipe: Recipe = serde_yaml::from_str(&recipe)?;
+    let json = serde_json::to_string(&recipe).unwrap();
+    fs::write("recipe.json", json);
     let text = recipe.render().unwrap();
     fs::write("out.html", text).unwrap();
     Ok(())
